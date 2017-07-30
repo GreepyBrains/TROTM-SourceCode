56876d83160197e44007c48e220d36741f9c2cf1
GreepyBrains <30568053+GreepyBrains@users.noreply.github.com>
2017-07-30 20:05:34 +0200

    Final Commit
    Finally i got it.
    :)

---
+++ Scripts/Only Intro/OutroEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutroEnd : MonoBehaviour 
+{
+	GameObject introEnder;
+
+	void Start () 
+	{
+		introEnder = transform.GetChild (0).gameObject;
+		introEnder.SetActive (false);
+		StartCoroutine (EndIntro ());
+	}
+
+	IEnumerator EndIntro()
+	{
+		yield return new WaitForSeconds (6f);
+		introEnder.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void OpenTwitter()
+	{
+		Application.OpenURL("https://twitter.com/GreepyBrainsDev");
+	}
+}
